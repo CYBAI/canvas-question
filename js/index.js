d19c1f375059560fe15ba70cab40719a841ecae4
CYBAI <cyb.ai.815@gmail.com>
2016-05-09 02:40:38 +0800

    Make images randomly show up

--- js/index.js
+++ js/index.js
@@ -4,8 +4,20 @@ const boxContext = box.getContext('2d');
 let rAF;
 let counter = 0;
 let lasttime = 0;
+
+const shuffleArray = (array) => {
+  let m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+};
+
 let imageLoaded = 0;
-const images = Array.from(new Array(12), (val, idx) => {
+const images = shuffleArray(Array.from(new Array(12), (val, idx) => {
   const img = new Image();
   img.src = `images/${idx}.png`;
   img.onload = () => {
@@ -15,12 +27,14 @@ const images = Array.from(new Array(12), (val, idx) => {
     }
   };
   return img;
-});
+}));
+
+const order = shuffleArray(Array.from(new Array(12), (val, idx) => idx));
 
 function draw() {
   const thisImg = images[counter];
-  const x = (box.width / 4) * (counter % 4);
-  const y = (box.height / 3) * Math.floor((counter + 1) % 3);
+  const x = (box.width / 4) * (order[counter] % 4);
+  const y = (box.height / 3) * Math.floor((order[counter] + 1) % 3);
   boxContext.drawImage(
     thisImg,
     x, y,
